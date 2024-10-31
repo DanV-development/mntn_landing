f30becbba3e9293e10ce187dc3b1274ef16935e3
Воробьев Данил Сергеевич <dvorobev@@ispras.ru>
2024-10-31 10:04:34 +0300

    Added separator effects

--- js/app.js
+++ js/app.js
@@ -3,7 +3,21 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 let rightItems = gsap.utils.toArray(".gallery__right");
 let leftItems = gsap.utils.toArray(".gallery__left");
-const scrollDownBtn = document.querySelector(".hero-section__scroll-down")
+let pageContentSeparatorLines = gsap.utils.toArray(
+    ".gallery .h3-separator__line"
+);
+let pageContentSeparatorText = gsap.utils.toArray(".gallery .h3-separator h3");
+
+const headerTextContent = document.querySelector(
+    ".hero-section__header-text-content"
+);
+const headerTextSeparatorLine = headerTextContent.querySelector(
+    ".h3-separator__line"
+);
+const headerTextH3SeparatorText = headerTextContent.querySelector(
+    ".h3-separator"
+).querySelector("h3");
+const scrollDownBtn = document.querySelector(".hero-section__scroll-down");
 
 ScrollSmoother.create({
     wrapper: ".wrapper",
@@ -37,16 +51,90 @@ rightItems.forEach(item => {
             scrub: true
         }
     })
-})
+});
+
+pageContentSeparatorLines.forEach(item => {
+    gsap.fromTo(item, { x: -50, opacity: 0 },
+    {
+        x: 0,
+        opacity: 1,
+        scrollTrigger: {
+            trigger: item,
+            start: "-800",
+            end: "-200",
+            scrub: true,
+        },
+    }
+    )
+});
+
+pageContentSeparatorText.forEach(item => {
+    gsap.fromTo(item, { x: 50, opacity: 0 },
+    {
+        x: 0,
+        opacity: 1,
+        scrollTrigger: {
+            trigger: item,
+            start: "-800",
+            end: "-200",
+            scrub: true,
+        },
+    }
+    )
+});
+
+gsap.fromTo(headerTextContent, { opacity: 1 }, {
+    opacity: 0,
+    scrollTrigger: {
+        trigger: headerTextContent,
+        start: "top",
+        end: "bottom",
+        scrub: true,
+    }
+});
+
+gsap.fromTo(
+    headerTextSeparatorLine,
+    { x: 0, opacity: 1 },
+    {
+        x: -50,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: headerTextContent,
+            start: "-20",
+            end: "400",
+            scrub: true,
+        },
+    }
+);
+
+gsap.fromTo(
+    headerTextH3SeparatorText,
+    { x: 0, opacity: 1 },
+    {
+        x: 50,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: headerTextH3SeparatorText,
+            start: "-20",
+            end: "400",
+            scrub: true,
+        }
+    }
+);
 
 document.addEventListener("scroll", function() {
-    let documentHeight = document.documentElement.scrollHeight - window.innerHeight;
-    
+    let documentHeight = (
+        document.documentElement.scrollHeight - window.innerHeight
+    );
+
     let scrollTop = window.scrollY;
 
     let scrollPercent = scrollTop / documentHeight;
 
-    let activeIndicator = document.querySelector(".page-scroll-indicatior__active-section");
+    let activeIndicator = document.querySelector(
+        ".page-scroll-indicatior__active-section"
+    );
 
     if (scrollPercent * 100 < 30) {
         activeIndicator.style.top = "0";
@@ -61,7 +149,9 @@ document.addEventListener("scroll", function() {
 
 scrollDownBtn.addEventListener("click", function() {
         // Общая высота страницы за вычетом высоты видимой области
-        let documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+        let documentHeight = (
+            document.documentElement.scrollHeight - window.innerHeight
+        );
 
         // Высчитываем 25% от общей высоты страницы
         let scrollAmount = documentHeight * 0.3;
